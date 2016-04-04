181d7f41cfecf6947203f630e66f11d68b30821d
欧伟科 <brainor@qq.com>
2016-04-04 11:29:39 +0800

    把所有文件都放进去了

---
+++ app/src/main/java/com/brainor/myfirstapp/HWR.java
@@ -0,0 +1,175 @@
+package com.brainor.myfirstapp;
+
+/**
+ * Created by 欧伟科 on 2016/4/3.
+ */
+
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.security.PublicKey;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Objects;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+class 网络 extends 数据 {
+    public HttpURLConnection Request;
+    public java.util.List<String> cookies;
+    public String[] 建立连接(String URL,String PostData) throws Exception {
+        java.net.URL url=new java.net.URL(URL);
+        Request=(HttpURLConnection)url.openConnection();
+        Request.setRequestMethod("POST");
+        Request.setDoOutput(true);
+        Request.setRequestProperty("Content-type", "application/x-www-form-urlencoded");
+        byte[] byteArray= PostData.getBytes("UTF-8");
+        Request.setRequestProperty("ContentLength", String.valueOf(byteArray.length));
+        if (!PostData.equals("")){
+            OutputStream dataStreamRequest=Request.getOutputStream();
+            dataStreamRequest.write(byteArray);
+            dataStreamRequest.flush();
+            dataStreamRequest.close();
+        }
+        cookies=Request.getHeaderFields().get("Set-Cookie");
+        return new String[]{"成功"};
+    }
+
+    public String[] 获得Ccookie() throws Exception{
+        String[] 错误代码=建立连接("https://its.pku.edu.cn/cas/login", 学生.postData());
+        if (Objects.equals(错误代码[0], "错误")) return 错误代码;
+        InputStream Response = Request.getInputStream();
+        Response.close();
+        return new String[]{"成功"};
+    }
+    public String[] 连接(String 连接类型) throws Exception {
+        String[] 错误代码=获得Ccookie();
+        if (Objects.equals(错误代码[0], "错误")) return 错误代码;
+        建立连接("https://its.pku.edu.cn/netportal/" + 连接类型,"");
+        InputStream Response = Request.getInputStream();
+        String 文档=分析网页(Response);
+        Response.close();
+        return 返回信息(文档);
+    }
+    private String 分析网页(InputStream Response){
+        Scanner sc=new Scanner(Response);
+        String ResponseFromServer="";
+        while(sc.hasNextLine()){
+            ResponseFromServer+=sc.nextLine();
+        }
+        sc.close();
+        return ResponseFromServer;
+    }
+    public  String[] 返回信息(String 文档) throws Exception {
+        int i=文档.indexOf("<!--IPGWCLIENT_START") + 21;
+        if(i==20){
+            return new String[] { "错误", "用户名或者密码错误" };
+        }
+        int j = 文档.indexOf("IPGWCLIENT_END-->");
+        String Content = 文档.substring(i, j - i - 1);
+        if (Content.contains("SUCCESS=NO"))//出现了各种问题
+        {
+            Pattern p= Pattern.compile("(?<=REASON=).*");
+            String 原因 = p.matcher(Content).group();
+            switch (原因)
+            {
+                case "当前连接数超过预定值":
+                    return 获得IP(文档);
+                default://不是当前的原因
+                    return new String[] { "错误", 原因 };
+            }
+        }
+        else
+        {
+            return 信息翻译(Content);
+        }
+    }
+    public String[] 获得IP(String 文档) throws Exception {
+        String postData;
+        int i = 文档.indexOf("\"messages\" value") + 18;
+        int j = 文档.indexOf("\">", i);
+        学生.value = 文档.substring(i, j - i);
+
+        postData = "messages=" + 学生.value + "&operation=get_disconnectip_err&from=cas&uid=" + 学生.学号 + "&timeout=1&range=2";
+
+        建立连接("https://its.pku.edu.cn/netportal/ipgw.ipgw?" + postData,"");//POST不行GET却可以
+        InputStream Response = Request.getInputStream();
+        String IP地址=分析网页(Response);
+        Response.close();
+
+        //断开指定连接
+        Pattern[] pattern=new Pattern[2];
+        pattern[0]=Pattern.compile("(?<=td2>)\\d+(\\.\\d+){3}?");//IP地址
+        pattern[1]=Pattern.compile("(?<=td2>)[\\d\\-]+\\s[\\d:]+\\.0");//时间
+        Matcher[] MC=new Matcher[2];
+        for (i=0;i<2;i++)             MC[i]=pattern[i].matcher(IP地址);
+        String[] IP=new String[2*MC[0].groupCount()];
+        for(i=0;i<MC[0].groupCount();i++){
+            IP[i*2]=MC[0].group(i);
+            IP[i*2+1]=MC[1].group(i);
+        }
+        Date 时间;
+        for(i=0;i<MC[0].groupCount();i++){
+            时间=new SimpleDateFormat().parse(IP[i * 2 + 1]);
+            IP[2*i+1]=new SimpleDateFormat("M-d H:mm:ss").format(时间);
+        }
+        return IP;
+    }
+    public String[] 断开指定连接(String IP) throws Exception {
+        //断开第一个连接
+        String postData = "messages=" + 学生.value + "&operation=disconnectip_err&from=cas&uid=" + 学生.学号 + "&timeout=1&range=1&disconnectip=" + IP;
+        建立连接("http://its.pku.edu.cn/netportal/ipgw.ipgw?" + postData,"");
+
+        InputStream Response = Request.getInputStream();
+        String 文档=分析网页(Response);
+        Response.close();
+        return 返回信息(文档);
+    }
+    private String[] 信息翻译(String Content){
+        Content=Content.replace("STATE=","状态\t\t");
+        Content = Content.replace("connected", "已连接");
+        Content = Content.replace("USERNAME=", "用户名\t\t");
+        Content = Content.replaceAll("FIXRATE=\\S*\\s", "");
+        Content = Content.replace("FR_DESC_CN=", "包月状态\t");
+        Content = Content.replaceAll("FR_DESC_EN=\\S*\\s", "");
+        Content = Content.replace("FR_TIME=", "已用时长\t");
+        Pattern pattern=Pattern.compile("(?<=已用时长\t)[\\d\\.]*(?=\\s)");
+        String 时长=pattern.matcher(Content).group();
+        if(时长.length()>0){//后期添加吧
+
+        }
+        Content = Content.replace("SCOPE=", "访问范围\t");
+        if (Content.contains("domestic")) Content = Content.replace("domestic", "免费流量");
+        else Content = Content.replace("international", "收费流量");
+        Content = Content.replace("CONNECTIONS=", "当前连接数\t");
+        Content = Content.replace("BALANCE=", "余额\t\t");
+        Content = Content.replace("IP=", "IP地址\t\t");
+        Content = Content.replace("MESSAGE=", "信息\t\t");
+        return Content.split(" ");
+    }
+
+}
+class 数据{
+    public static class 用户信息{
+        public String 学号;
+        public String 密码;
+        private fwrd 类型=fwrd.free;
+        public String value;
+        private String code="|;kiDrqvfi7d$v0p5Fg72Vwbv2;|";
+        public String postData(){ return "fwrd=" + 类型 + "&username1=" + 学号 + "&password=" + 密码 + "&username=" + 学号 + code + 密码 + code + 类型.index; }
+        public 用户信息(String 学号,String 密码){
+            this.学号= 学号;
+            this.密码= 密码;
+        }
+
+    }
+    enum fwrd {
+        fee((short)11),        free((short)12),        pkuwireless((short)14);
+        private short index;
+        fwrd(short index){
+            this.index=index;
+        }
+    }
+    public 用户信息 学生;
+}
--- app/src/main/java/com/brainor/myfirstapp/MainActivity.java
+++ app/src/main/java/com/brainor/myfirstapp/MainActivity.java
@@ -5,11 +5,24 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.AndroidCharacter;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    数据.用户信息[] 学生信息=new 数据.用户信息[2];
+    网络 web=new 网络();
+    //各种按钮
+    RadioButton[] radioButton=new RadioButton[2];
+    TextView textBlock;
+    Button[] button=new Button[3];
+    Spinner 学号;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,14 +31,22 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        //增加用户信息列表
+        学生信息[0] = new 数据.用户信息("1301110110","Oudanyi6456");
+        学生信息[1] = new 数据.用户信息("gcuspea", "phyfsc508");
+
+        // Get our button from the layout resource,
+        // and attach an event to it
+        radioButton[0] = (RadioButton)findViewById(R.id.radioButton1);
+        radioButton[1] =(RadioButton)findViewById(R.id.radioButton2);
+        textBlock =(TextView) findViewById(R.id.信息文本);
+        button[0] = (Button)findViewById(R.id.button1);
+        button[1] = (Button)findViewById(R.id.button2);
+        button[2] = (Button)findViewById(R.id.button3);
+        学号 = (Spinner)findViewById(R.id.学生信息);
+
+        ArrayAdapter<String> adapter=new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item);
+        
     }
 
     @Override
