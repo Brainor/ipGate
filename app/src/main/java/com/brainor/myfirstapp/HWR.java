b88fd0c6004ea9eff29460d6a99650aa538f0e64
欧伟科 <brainor@qq.com>
2016-04-29 03:23:29 +0800

    为什么断开指定连接有问题?

--- app/src/main/java/com/brainor/myfirstapp/HWR.java
+++ app/src/main/java/com/brainor/myfirstapp/HWR.java
@@ -14,6 +14,7 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
@@ -21,69 +22,88 @@ import java.util.List;
 import java.util.Objects;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
 import static android.text.TextUtils.join;
 
 class network extends data {
-    public HttpURLConnection Request;
-
     public network(){
         CookieHandler.setDefault(new CookieManager());
     }
-    public String[] 建立连接(String URL, String postData) throws MalformedURLException {
-        java.net.URL url=new java.net.URL(URL);
-        try {
-            Request=(HttpURLConnection)url.openConnection();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        Request.setRequestProperty("Cookie", join(";", ((CookieManager)CookieHandler.getDefault()).getCookieStore().getCookies()));
-        Request.setDoInput(true);
-        Request.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
-        if (!postData.equals("")){
-            Request.setDoOutput(true);
-            Request.setRequestProperty("Content-Length",Integer.toString(postData.length()));
-            try {
-                Request.setRequestMethod("POST");
-                java.io.DataOutputStream dataOutputStream = new java.io.DataOutputStream(Request.getOutputStream());
-                dataOutputStream.write(postData.getBytes(StandardCharsets.UTF_8));
-                dataOutputStream.flush();
-                dataOutputStream.close();
-            } catch (Exception ex) {
-                return new String[]{"错误", ex.getMessage()};
-            }
-        }
-        return new String[]{"成功"};
+    public String[] 连接(String 连接类型){
+        String[] 连接信息=建立连接("https://its.pku.edu.cn/cas/login", 学生.postData());//获得cookies
+        if (Objects.equals(连接信息[0], "错误")) return 连接信息;
+        连接信息=建立连接("https://its.pku.edu.cn/netportal/" + 连接类型,"");
+        if (Objects.equals(连接信息[0], "错误")) return 连接信息;
+        return 返回信息(连接信息[1]);
     }
-
-    public String[] 获得Cookie() throws MalformedURLException {
-       String[] 错误代码=建立连接("https://its.pku.edu.cn/cas/login", 学生.postData());
-        if (Objects.equals(错误代码[0], "错误")) return 错误代码;
+    public String[] 断开指定连接(String IP) throws Exception {
+        String postData = "messages=" + 学生.value + "&operation=disconnectip_err&from=cas&uid=" + 学生.学号 + "&timeout=1&range=1&disconnectip=" + IP;
+        String[] 连接信息=建立连接("http://its.pku.edu.cn/netportal/ipgw.ipgw?" + postData,"");
+        return 返回信息(连接信息[1]);
+    }
+    /*public String[] 获得Cookie(){
+        String[] 连接信息=建立连接("https://its.pku.edu.cn/cas/login", 学生.postData());
+        if (Objects.equals(连接信息[0], "错误")) return 连接信息;
         try {
             Request.getInputStream().close();
         } catch (IOException e) {
             e.printStackTrace();
         }
         return new String[]{"成功"};
-    }
-    public String[] 连接(String 连接类型) throws Exception {
-        String[] 错误代码=获得Cookie();
-        if (Objects.equals(错误代码[0], "错误")) return 错误代码;
-        建立连接("https://its.pku.edu.cn/netportal/" + 连接类型,"");
-        String 文档=分析网页(Request);
-        return 返回信息(文档);
-    }
-    private String 分析网页(HttpURLConnection request) throws IOException {
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(request.getInputStream()));
-        String line,ResponseFromServer="";
-        while((line = bufferedReader.readLine()) != null ){
-            ResponseFromServer+=line;
+    }*/
+    /**
+     * 连网并返回信息
+     * @param URL 要连接的网址
+     * @param postData POST方法的数据
+     * @return 错误的话, 第一个值为"错误", 第二个值为错误信息; 正确的话, 第一个值为"正确", 第二个值为网页信息.
+     */
+    public String[] 建立连接(String URL, String postData) {
+        java.net.URL url;
+        try {
+            url = new java.net.URL(URL);
+        } catch (MalformedURLException e) {
+            return new String[]{"错误",e.getMessage()};
         }
-        bufferedReader.close();
+        HttpURLConnection request;
+        try {
+            request = (HttpURLConnection)url.openConnection();
+        } catch (IOException e) {
+            return new String[]{"错误",e.getMessage()};
+        }
 
-        return ResponseFromServer;
-
+        request.setRequestProperty("Cookie", join(";", ((CookieManager)CookieHandler.getDefault()).getCookieStore().getCookies()));
+        request.setDoInput(true);
+        request.setReadTimeout(300000);
+        if (!postData.equals("")){
+            request.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+            request.setDoOutput(true);
+            request.setRequestProperty("Content-Length",Integer.toString(postData.length()));
+            try {
+                request.setRequestMethod("POST");
+                java.io.DataOutputStream dataOutputStream = new java.io.DataOutputStream(request.getOutputStream());
+                dataOutputStream.write(postData.getBytes(StandardCharsets.UTF_8));
+                dataOutputStream.flush();
+                dataOutputStream.close();
+            } catch (IOException e) {
+                return new String[]{"错误",e.getMessage()};
+            }
+        }
+//分析网页部分
+        String line, ResponseFromServer = "";
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(request.getInputStream()));
+            while ((line = bufferedReader.readLine()) != null) {
+                ResponseFromServer += line+"\n";
+            }
+            bufferedReader.close();
+        }catch (IOException e){
+            return new String[]{"错误",e.getMessage()};
+        }
+        request.disconnect();
+        return new String[]{"成功",ResponseFromServer};
     }
-    public  String[] 返回信息(String 文档) throws Exception {
+
+    public  String[] 返回信息(String 文档){
         int i=文档.indexOf("<!--IPGWCLIENT_START ") + 21;
         if(i==20){
             return new String[] { "错误", "用户名或者密码错误" };
@@ -108,17 +128,23 @@ class network extends data {
             return 信息翻译(Content);
         }
     }
-    public String[] 获得IP(String 文档) throws Exception {
+
+    /**
+     * 超过最大连接数后, 得到IP地址
+     * @param 文档 利用文档里的信息获得IP地址
+     * @return 字符串数组, 第一项不是"正确"也不是"错误"
+     */
+    public String[] 获得IP(String 文档){
         String postData;
         int i = 文档.indexOf("\"messages\" value") + 18;
         int j = 文档.indexOf("\">", i);
-        学生.value = 文档.substring(i, j - 1);
-
+        学生.value = 学生.decode(文档.substring(i, j));
         postData = "messages=" + 学生.value + "&operation=get_disconnectip_err&from=cas&uid=" + 学生.学号 + "&timeout=1&range=2";
 
-        建立连接("https://its.pku.edu.cn/netportal/ipgw.ipgw?" + postData,"");//POST不行GET却可以
-        String IP地址=分析网页(Request);
+        String[] 连接信息=建立连接("https://its.pku.edu.cn/netportal/ipgw.ipgw?" + postData,"");//POST不行GET却可以
+        if (Objects.equals(连接信息[0], "错误")) return 连接信息;
 
+        String IP地址=连接信息[1];
         //断开指定连接
         List<String> IPList= new ArrayList<>();
         Matcher matcher=Pattern.compile("(?<=td2>)[^<].*?(?=</td>)").matcher(IP地址);
@@ -128,20 +154,18 @@ class network extends data {
         String[] IP= IPList.toArray(new String[IPList.size()]);//IP,免费,时间
 
         Date 时间;
-        for(i=0;i<IP.length/3;i++){
-            时间=new SimpleDateFormat("y-MM-dd kk:mm:ss.S").parse(IP[i * 3 + 2]);
-            IP[3*i+2]=new SimpleDateFormat("M-d H:mm:ss").format(时间);
+        try {
+            for(i=0;i<IP.length/3;i++) {
+
+                时间 = new SimpleDateFormat("y-MM-dd kk:mm:ss.S").parse(IP[i * 3 + 2]);
+                IP[3 * i + 2] = new SimpleDateFormat("M-d H:mm:ss").format(时间);
+            }
+        } catch (ParseException e) {
+            return new String[]{"错误",e.getMessage()};
         }
         return IP;
     }
-    public String[] 断开指定连接(String IP) throws Exception {
-        //断开第一个连接
-        String postData = "messages=" + 学生.value + "&operation=disconnectip_err&from=cas&uid=" + 学生.学号 + "&timeout=1&range=1&disconnectip=" + IP;
-        建立连接("http://its.pku.edu.cn/netportal/ipgw.ipgw?" + postData,"");
 
-        String 文档=分析网页(Request);
-        return 返回信息(文档);
-    }
     private String[] 信息翻译(String Content){
         Content=Content.replace("STATE=","状态\t");
         Content = Content.replace("connected", "已连接");
@@ -174,16 +198,18 @@ class data {//数据
         public String 密码;
         private fwrd 类型=fwrd.free;
         public String value;
-        private final String code = "|;kiDrqvfi7d$v0p5Fg72Vwbv2;|";
+        private final String code = decode("|;kiDrqvfi7d$v0p5Fg72Vwbv2;|");
         public String postData(){
-            String data="username1=" + 学号 + "&password=" + 密码 + "&pwd_t=密码&fwrd="+类型.name()+"&username=" + 学号 + code + 密码 + code + 类型.index;
+            return "username1=" + 学号 + "&password=" + 密码 + "&pwd_t="+decode("密码")+"&fwrd="+类型.name()+"&username=" + 学号 + code + 密码 + code + 类型.index;
+        }
+        public String decode(String data){
+            String 编码data="";
             try {
-                data= URLEncoder.encode(data, StandardCharsets.UTF_8.name());
+                编码data=URLEncoder.encode(data, StandardCharsets.UTF_8.name());
             } catch (UnsupportedEncodingException e) {
                 e.printStackTrace();
             }
-            data=data.replace("%26","&").replace("%3D","=");
-            return data;
+            return 编码data;
         }
         public userInfo(String 学号, String 密码){
             this.学号= 学号;
